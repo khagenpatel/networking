0abcf24121ead1c80aca6630abbc75deb713b214
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-12 11:41:03 -0400

    Update parse_ip_route.py

--- parse_ip_route.py
+++ parse_ip_route.py
@@ -1,37 +1,34 @@
 import re
 import csv
-
+from datetime import datetime
+timestamp = datetime.now().strftime('%Y-%m-%d-%H%M%S')
 # Read input from the file
 with open('input.txt', 'r') as file:
     routing_table = file.read()
 
-# Split the routing table into sections by 'show ip route connected'
-sections = re.split(r'show ip route connected', routing_table)
+# Split the routing table into sections
+sections = re.split(r'(?<=\n\n)', routing_table)
 
 # Process each section and save output to CSV
-with open('output.csv', 'w', newline='') as csvfile:
+with open(f'output-{timestamp}.csv', 'w', newline='') as csvfile:
     fieldnames = ['hostname', 'subnet', 'interface']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
     writer.writeheader()
 
     # Iterating through each section
     for section in sections:
-        # Extracting hostname from the section
-        hostname_match = re.search(r'([^\n]+)#', section)
+        # Extracting hostname from above the output section
+        hostname_match = re.search(r'([\w#-]+)\nshow ip route', section)
         if hostname_match:
             hostname = hostname_match.group(1).strip()
         else:
-            hostname_match = re.search(r'show ip route connected\s+([\w#-]+)', section)
-            if hostname_match:
-                hostname = hostname_match.group(1).strip()
-            else:
-                continue  # Skip section if hostname is not found
+            continue  # Skip section if hostname is not found
 
-        # Extracting entries from the section
-        entries = re.findall(r'^[CL]\s+(\S+)\s+is directly connected,\s+(\w+)', section, re.MULTILINE)
+        # Extracting entries from the output section
+        entries = re.findall(r'C\s+(\S+)\s+is directly connected,\s+(\w+)', section)
 
-        # Iterating through each line in the section
+        # Iterating through each entry in the output section
         for entry in entries:
             subnet, interface = entry
+
             writer.writerow({'hostname': hostname, 'subnet': subnet, 'interface': interface})
