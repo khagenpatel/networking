c46ce6bf2a745a51408ffb4268b850337f58cbe8
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-12 13:39:29 -0400

    Update parse_ip_route.py

--- parse_ip_route.py
+++ parse_ip_route.py
@@ -1,43 +1,69 @@
 import re
 import csv
-from datetime import datetime
+import datetime
 
-# Read input from the file
-with open('input.txt', 'r') as file:
-    routing_table = file.read()
+input_file = "input.txt"
+output_file = f"output_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
 
-# Split the routing table into sections
-sections = re.split(r'\n\n', routing_table)
+non_compliant_sections = []
+compliant_sections = []
 
-# Process each section and save output to CSV
-fieldnames = ['hostname', 'subnet', 'interface', 'timestamp']
-with open('output.csv', 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+# Regex pattern to match the hostname line
+hostname_pattern = r"^([^#]+)#"
 
-    # Iterate through each section
-    for section in sections:
-        # Extract hostname from above the output section
-        hostname_match = re.search(r'([\w#-]+)\nshow ip route', section)
-        if hostname_match:
-            hostname = hostname_match.group(1).strip()
+# Regex pattern to match the subnet and interface lines
+subnet_pattern = r"^C\s+([\d.]+/\d+)\s+is directly connected,\s+([\w/]+)"
+
+with open(input_file, 'r') as file:
+    lines = file.readlines()
+
+# Iterate over the lines in the input file
+i = 0
+while i < len(lines):
+    line = lines[i].strip()
+    if re.match(hostname_pattern, line):
+        hostname = re.match(hostname_pattern, line).group(1)
+        i += 1
+        if i < len(lines) and lines[i].strip() == "show ip route connected":
+            i += 1
+            if i < len(lines) and not lines[i].startswith(" ") and not lines[i].startswith("show ip route"):
+                non_compliant_sections.append((hostname, "show ip route connected not found"))
+                i += 1
+            else:
+                i += 1
+                section = []
+                while i < len(lines) and lines[i].strip() != "":
+                    line = lines[i].strip()
+                    if re.match(subnet_pattern, line):
+                        subnet = re.match(subnet_pattern, line).group(1)
+                        interface = re.match(subnet_pattern, line).group(2)
+                        section.append((hostname, subnet, interface))
+                    else:
+                        non_compliant_sections.append((hostname, "Invalid line format"))
+                    i += 1
+                compliant_sections.extend(section)
         else:
-            # Print the non-compliant section
-            print("Non-compliant section:")
-            print(section)
-            continue  # Skip section if hostname is not found
+            non_compliant_sections.append((hostname, "Invalid section format"))
+    else:
+        non_compliant_sections.append(("Unknown", "Invalid line format"))
+        i += 1
 
-        # Extract entries from the output section
-        entries = re.findall(r'C\s+(\S+)\s+is directly connected,\s+(\w+)', section)
+# Print non-compliant sections
+print("Non-compliant sections:")
+for section in non_compliant_sections:
+    print(f"{section[0]} - Reason: {section[1]}")
+print()
 
-        # Iterate through each entry in the output section
-        for entry in entries:
-            subnet, interface = entry
-            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            writer.writerow({'hostname': hostname, 'subnet': subnet, 'interface': interface, 'timestamp': timestamp})
-            
-            # Print the current entry
-            print(f"Hostname: {hostname}, Subnet: {subnet}, Interface: {interface}")
+# Print compliant sections
+print("Compliant sections:")
+for section in compliant_sections:
+    print(f"{section[0]} | {section[1]} | {section[2]}")
+print()
 
-# Output CSV file is saved
-print("Output saved to output.csv")
+# Save compliant sections to CSV file
+with open(output_file, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["hostname", "subnet", "interface"])
+    writer.writerows(compliant_sections)
+
+print(f"Output saved to {output_file}")
