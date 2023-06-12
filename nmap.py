8f1a1a17c533b6982bca0643e1a40665669232ce
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-12 14:46:02 -0400

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,27 +1,27 @@
 import subprocess
 import csv
 
-def get_live_hosts(ip_range):
-    command = f"nmap -sn {ip_range}"
+def get_live_host(ip):
+    command = f"nmap -sn {ip}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    output = result.stdout.strip()
-    live_hosts = []
+    output = result.stdout
     
-    lines = output.split("\n")
-    for line in lines:
+    live_host = "N/A"
+    live_ip = "N/A"
+    
+    for line in output.splitlines():
         if "Nmap scan report for" in line:
-            host_line = line.split("Nmap scan report for ")[1]
-            host = host_line.split(" (")[0]
-            ip = host_line.split(" (")[1].split(")")[0]
-            live_hosts.append((host, ip))
+            parts = line.split()
+            live_host = parts[4]
+            live_ip = parts[5][1:-1]
+            break
     
-    return live_hosts
+    return live_host, live_ip
 
 def main():
     with open('input.csv', 'r') as input_file:
         reader = csv.reader(input_file)
         header = next(reader)  # Read the header row
-        ip_range_index = header.index('Subnet')
         data = list(reader)
     
     with open('output.csv', 'w', newline='') as output_file:
@@ -29,10 +29,11 @@ def main():
         writer.writerow(header + ['Live_Host', "Live_host's_IP"])
         
         for row in data:
-            ip_range = row[ip_range_index]
-            live_hosts = get_live_hosts(ip_range)
-            live_host_info = live_hosts.pop(0) if live_hosts else ("N/A", "N/A")
-            writer.writerow(row + live_host_info)
+            hostname = row[0]
+            subnet = row[1]
+            interface = row[2]
+            live_host, live_ip = get_live_host(subnet)
+            writer.writerow([hostname, subnet, interface, live_host, live_ip])
     
     print("Output saved to output.csv")
 
