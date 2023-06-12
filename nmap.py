643425541ab02477a641ce25551a44795e35fd22
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-12 14:40:11 -0400

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,32 +1,40 @@
 import subprocess
 import csv
 
-def get_live_host(hostname, ip):
-    command = f"nmap -sn {ip}"
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+def get_live_hosts(ip_range):
+    command = f"nmap -sn {ip_range}"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
     output = result.stdout.strip()
-    if "Host is up" in output:
-        live_host = hostname.strip("#")
-        live_ip = output.split("for ")[1].split()[0]
-        return live_host, live_ip
-    else:
-        return None, None
+    live_hosts = []
+    
+    lines = output.split("\n")
+    for line in lines:
+        if "Nmap scan report for" in line:
+            host_line = line.split("Nmap scan report for ")[1]
+            host = host_line.split(" (")[0]
+            ip = host_line.split(" (")[1].split(")")[0]
+            live_hosts.append((host, ip))
+    
+    return live_hosts
 
-input_file = "input.csv"
-output_file = "live_hosts.csv"
+def main():
+    with open('input.csv', 'r') as input_file:
+        reader = csv.reader(input_file)
+        header = next(reader)  # Read the header row
+        ip_range_index = header.index('Subnet')
+        data = list(reader)
+    
+    with open('output.csv', 'w', newline='') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(header + ['Live_Host', "Live_host's_IP"])
+        
+        for row in data:
+            ip_range = row[ip_range_index]
+            live_hosts = get_live_hosts(ip_range)
+            live_host_info = live_hosts.pop(0) if live_hosts else ("N/A", "N/A")
+            writer.writerow(row + live_host_info)
+    
+    print("Output saved to output.csv")
 
-with open(input_file, "r") as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader)  # Skip the header row
-
-    with open(output_file, "w", newline="") as output_file:
-        csv_writer = csv.writer(output_file)
-        csv_writer.writerow(["Hostname", "Subnet", "Interface", "Live Host", "Live IP"])
-
-        for row in csv_reader:
-            hostname, subnet, interface = row
-            live_host, live_ip = get_live_host(hostname, subnet)
-            if live_host and live_ip:
-                csv_writer.writerow([hostname, subnet, interface, live_host, live_ip])
-
-print(f"Output saved to {output_file}.")
+if __name__ == "__main__":
+    main()
