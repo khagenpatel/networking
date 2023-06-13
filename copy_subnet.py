e0cf4a7cc2c11834ca9506e825f381d631228618
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-13 09:49:42 -0400

    Update copy_subnet.py

--- copy_subnet.py
+++ copy_subnet.py
@@ -2,49 +2,28 @@ import csv
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 
-def convert_csv_to_excel(csv_file_path, excel_file_path):
-    # Read the CSV file
-    rows = []
-    with open(csv_file_path, 'r') as file:
-        reader = csv.reader(file)
-        rows = list(reader)
+# Initialize Workbook
+wb = Workbook()
+ws = wb.active
 
-    # Create a new workbook
-    workbook = Workbook()
-    sheet = workbook.active
+# Open CSV file and write to Excel
+with open('input.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        ws.append(row)
 
-    # Write rows to the Excel file
-    for row in rows:
-        sheet.append(row)
+# Highlight duplicates
+subnets = {}
+red_fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
 
-    # Highlight duplicate values
-    duplicate_fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
-
-    data = sheet.values
-    columns = next(data)
-    seen = set()
-    duplicate_indices = []
-
-    for index, row in enumerate(data, start=2):
-        key = tuple(row)
-        if key in seen:
-            duplicate_indices.append(index)
+for row in ws.iter_rows(min_row=2, min_col=2, max_col=2):
+    for cell in row:
+        if cell.value in subnets:
+            # Highlight this cell and the earlier cell that had the same value
+            cell.fill = red_fill
+            subnets[cell.value].fill = red_fill
         else:
-            seen.add(key)
+            subnets[cell.value] = cell
 
-    for col in sheet.iter_cols(min_row=2, max_row=sheet.max_row):
-        for cell in col:
-            if cell.row in duplicate_indices:
-                cell.fill = duplicate_fill
-
-    # Save the Excel file
-    workbook.save(excel_file_path)
-
-    print(f"CSV file '{csv_file_path}' has been converted to Excel file '{excel_file_path}' with highlighted duplicates.")
-
-# Specify the file paths
-csv_file_path = 'your_csv_file_path.csv'
-excel_file_path = 'your_excel_file_path.xlsx'
-
-# Call the function
-convert_csv_to_excel(csv_file_path, excel_file_path)
+# Save the workbook to a file
+wb.save("output.xlsx")
