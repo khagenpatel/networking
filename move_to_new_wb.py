27a440e433bc0f719e83a1e9b366ecd315c2ba1a
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-13 10:34:15 -0400

    Update move_to_new_wb.py

--- move_to_new_wb.py
+++ move_to_new_wb.py
@@ -1,5 +1,8 @@
 import csv
 from openpyxl import Workbook
+from datatime import datetime
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
 # Initialize Workbook
 wb = Workbook()
@@ -22,23 +25,23 @@ with open('input.csv', mode='r') as csv_file:
         ws_all.append(row)
 
 # Check for duplicates and move to "Duplicates" sheet
-subnets = {}
+live_hosts = {}
 rows_to_delete = []
 
-for row in ws_all.iter_rows(min_row=2, min_col=2, max_col=2):
+for row in ws_all.iter_rows(min_row=2, min_col=4, max_col=4):  # Live_Host is the 4th column
     for cell in row:
-        if cell.value in subnets:
+        if cell.value in live_hosts:
             # If this value has been seen before, it is a duplicate and should be moved
             duplicate_row = ws_all[cell.row]
             ws_duplicates.append([c.value for c in duplicate_row])
             rows_to_delete.append(cell.row)
         else:
             # First time seeing this value, just remember it
-            subnets[cell.value] = cell
+            live_hosts[cell.value] = cell
 
 # Delete rows from 'All Data' after moving to 'Duplicates'
 for row_id in sorted(rows_to_delete, reverse=True):
     ws_all.delete_rows(row_id)
 
 # Save the workbook to a file
-wb.save("output.xlsx")
+wb.save(f"output-{timestamp}.xlsx")
