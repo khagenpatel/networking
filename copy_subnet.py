aa196e603345bcf5c40ef2f0986a2284885599da
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-13 09:43:21 -0400

    Update copy_subnet.py

--- copy_subnet.py
+++ copy_subnet.py
@@ -1,37 +1,50 @@
 import csv
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 
-def move_duplicates_to_another_tab(file_path):
+def convert_csv_to_excel(csv_file_path, excel_file_path):
     # Read the CSV file
-    with open(file_path, 'r') as file:
-        reader = csv.DictReader(file)
+    rows = []
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
         rows = list(reader)
 
-    # Find duplicate subnets
-    duplicate_subnets = set()
-    unique_subnets = set()
+    # Create a new workbook
+    workbook = Workbook()
+    sheet = workbook.active
 
+    # Write rows to the Excel file
     for row in rows:
-        subnet = row['Subnet']
-        if subnet in unique_subnets:
-            duplicate_subnets.add(subnet)
+        sheet.append(row)
+
+    # Highlight duplicate values
+    duplicate_fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+
+    data = sheet.values
+    columns = next(data)
+    seen = set()
+    duplicate_indices = []
+
+    for index, row in enumerate(data, start=2):
+        key = tuple(row)
+        if key in seen:
+            duplicate_indices.append(index)
         else:
-            unique_subnets.add(subnet)
+            seen.add(key)
 
-    # Create a new CSV file for duplicates
-    duplicate_file_path = file_path.replace('.csv', '_duplicates.csv')
-    with open(duplicate_file_path, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=reader.fieldnames)
-        writer.writeheader()
+    for col in sheet.iter_cols(min_row=2, max_row=sheet.max_row):
+        for cell in col:
+            if cell.row in duplicate_indices:
+                cell.fill = duplicate_fill
 
-        # Write rows with duplicate subnets to the new file
-        for row in rows:
-            if row['Subnet'] in duplicate_subnets:
-                writer.writerow(row)
+    # Save the Excel file
+    workbook.save(excel_file_path)
 
-    print(f"Duplicates have been moved to {duplicate_file_path}.")
+    print(f"CSV file '{csv_file_path}' has been converted to Excel file '{excel_file_path}' with highlighted duplicates.")
 
-# Specify the file path
-file_path = 'your_file_path.csv'
+# Specify the file paths
+csv_file_path = 'your_csv_file_path.csv'
+excel_file_path = 'your_excel_file_path.xlsx'
 
 # Call the function
-move_duplicates_to_another_tab(file_path)
+convert_csv_to_excel(csv_file_path, excel_file_path)
