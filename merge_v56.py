9792831c959679f7877f0f63672b64fbcc426c5e
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-20 17:03:24 -0400

    Update merge_v56.py

--- merge_v56.py
+++ merge_v56.py
@@ -5,7 +5,8 @@ from openpyxl import load_workbook
 try:
     # Load the Excel file
     workbook = load_workbook('input1.xlsx')
-    sheet = workbook.active
+    sheet_name = 'Your_Sheet_Name'  # Replace 'Your_Sheet_Name' with the actual name of the worksheet
+    sheet = workbook[sheet_name]
 
     # Load the CSV file
     with open('input2.csv', 'r', encoding='utf-8-sig') as csvfile:  # Specify the encoding for the CSV file
@@ -21,7 +22,7 @@ try:
     # Iterate through the rows in the Excel file
     for row in sheet.iter_rows(min_row=2):
         live_host_full = row[3].value  # Extract the full Live_Host value
-        match = re.search(r'(\S+)\s', live_host_full)  # Extract the hostname using regex
+        match = re.search(r'\b(\w+)\b', live_host_full)  # Extract the hostname using regex
         if match:
             live_host = match.group(1).lower().encode('ascii', 'ignore').decode()  # Convert Live_Host to lowercase and ASCII
             if live_host in csv_data:
@@ -29,11 +30,11 @@ try:
                 values = csv_data[live_host]
 
                 # Add the values to the Excel file
-                sheet.cell(row=row[0].row, column=8).value = values[0]  # Owning Transaction Cycle
-                sheet.cell(row=row[0].row, column=9).value = values[1]  # CDR
-                sheet.cell(row=row[0].row, column=10).value = values[2]  # IT Business Service
-                sheet.cell(row=row[0].row, column=11).value = values[3]  # IT Service Instance
-                sheet.cell(row=row[0].row, column=12).value = values[4]  # ITSI Environment
+                row[7].value = values[0]  # Owning Transaction Cycle
+                row[8].value = values[1]  # CDR
+                row[9].value = values[2]  # IT Business Service
+                row[10].value = values[3]  # IT Service Instance
+                row[11].value = values[4]  # ITSI Environment
 
     # Save the modified Excel file
     workbook.save('output.xlsx')
