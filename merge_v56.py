c00ec722e40b82a4bf9887af33b01fb52daebe23
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-20 17:05:03 -0400

    Update merge_v56.py

--- merge_v56.py
+++ merge_v56.py
@@ -16,25 +16,27 @@ try:
         # Create a dictionary to store the mapping between hostnames and the other columns
         csv_data = {}
         for row in csvreader:
-            live_host = row[0].lower().encode('ascii', 'ignore').decode()  # Convert Live_Host to lowercase and ASCII
-            csv_data[live_host] = row[1:]  # Store the remaining columns in the dictionary
+            if len(row) > 0:
+                live_host = row[0].lower().encode('ascii', 'ignore').decode()  # Convert Live_Host to lowercase and ASCII
+                csv_data[live_host] = row[1:]  # Store the remaining columns in the dictionary
 
     # Iterate through the rows in the Excel file
     for row in sheet.iter_rows(min_row=2):
-        live_host_full = row[3].value  # Extract the full Live_Host value
-        match = re.search(r'\b(\w+)\b', live_host_full)  # Extract the hostname using regex
-        if match:
-            live_host = match.group(1).lower().encode('ascii', 'ignore').decode()  # Convert Live_Host to lowercase and ASCII
-            if live_host in csv_data:
-                # Get the values from the CSV file
-                values = csv_data[live_host]
+        if len(row) > 3:
+            live_host_full = row[3].value  # Extract the full Live_Host value
+            match = re.search(r'\b(\w+)\b', live_host_full)  # Extract the hostname using regex
+            if match:
+                live_host = match.group(1).lower().encode('ascii', 'ignore').decode()  # Convert Live_Host to lowercase and ASCII
+                if live_host in csv_data:
+                    # Get the values from the CSV file
+                    values = csv_data[live_host]
 
-                # Add the values to the Excel file
-                row[7].value = values[0]  # Owning Transaction Cycle
-                row[8].value = values[1]  # CDR
-                row[9].value = values[2]  # IT Business Service
-                row[10].value = values[3]  # IT Service Instance
-                row[11].value = values[4]  # ITSI Environment
+                    # Add the values to the Excel file
+                    row[7].value = values[0] if len(values) > 0 else None  # Owning Transaction Cycle
+                    row[8].value = values[1] if len(values) > 1 else None  # CDR
+                    row[9].value = values[2] if len(values) > 2 else None  # IT Business Service
+                    row[10].value = values[3] if len(values) > 3 else None  # IT Service Instance
+                    row[11].value = values[4] if len(values) > 4 else None  # ITSI Environment
 
     # Save the modified Excel file
     workbook.save('output.xlsx')
