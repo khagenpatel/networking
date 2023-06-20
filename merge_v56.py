1b8f492c44cd5c74e464ae9686d41d31fae4a9e9
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-20 17:31:59 -0400

    Update merge_v56.py

--- merge_v56.py
+++ merge_v56.py
@@ -9,7 +9,7 @@ ws = wb.active
 
 # Read csv file and create a dictionary for easier search
 csv_data = defaultdict(lambda: ["Not found"]*5)
-with open('input2.csv', 'r') as f:
+with open('input2.csv', 'r', encoding='ISO-8859-1') as f:
     csv_reader = csv.reader(f)
     headers = next(csv_reader)
     for row in csv_reader:
@@ -28,22 +28,24 @@ if live_host_idx is None:
     exit()
 
 # Append new columns in Excel
-ws.append(['Owning Transaction Cycle', 'CDR', 'IT Business Service', 'IT Service Instance', 'ITSI Environment'])
+for col, header in enumerate(['Owning Transaction Cycle', 'CDR', 'IT Business Service', 'IT Service Instance', 'ITSI Environment'], start=ws.max_column+1):
+    ws.cell(row=1, column=col, value=header)
 
 # Regular expression pattern for hostname
 pattern = re.compile(r"(.+?)[.\s]")
 
 # Process rows in Excel
-for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
-    match = pattern.match(row[live_host_idx-1].value)
+for row_idx in range(2, ws.max_row + 1):
+    cell = ws.cell(row=row_idx, column=live_host_idx)
+    match = pattern.match(cell.value)
     if match:
         live_host = match.group(1).lower()  # Get Live_Host and convert to lowercase
         new_data = csv_data[live_host]  # Get new data from csv file
-        for i, cell in enumerate(row[-5:], start=1):
-            cell.value = new_data[i-1]  # Update cell values
+        # Add new data to the end of the row
+        for col, data in enumerate(new_data, start=ws.max_column-len(new_data)+1):
+            ws.cell(row=row_idx, column=col, value=data)
     else:
-        print(f"Could not parse Live_Host value: {row[live_host_idx-1].value}")
-
+        print(f"Could not parse Live_Host value: {cell.value}")
 
 # Save workbook
 wb.save('input1_modified.xlsx')
