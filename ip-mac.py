def5f655213b9a2f339b7350c43149eec6c7bf7c
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-29 07:36:24 -0400

    Update ip-mac.py

--- ip-mac.py
+++ ip-mac.py
@@ -36,43 +36,18 @@ for device in devices:
         while shell.recv_ready():
             output += shell.recv(1024)
 
-        print("Output for {0}:\n{1}".format(device, output))  # Print the output for debugging
-
         # Retrieve the hostname based on the prompt
-        if ">" in output:
-            hostname_start = output.find(">") + 1
-            hostname_end = output.find("\n", hostname_start)
-        elif "#" in output:
-            hostname_start = output.find("#") + 1
-            hostname_end = output.find("\n", hostname_start)
-        else:
-            hostname_start = -1
-            hostname_end = -1
-
-        if hostname_start != -1 and hostname_end != -1:
-            hostname = output[hostname_start:hostname_end].strip()
-        else:
-            hostname = 'Unknown'
-
-        output_file.write("Hostname of {0}: {1}\n".format(device, hostname))
+        hostname = output.splitlines()[0].strip()
 
         # Retrieve the MAC addresses
-        mac_start = output.find('mac address-table')
-        mac_end = output.find('Type:', mac_start)
-        if mac_start != -1 and mac_end != -1:
-            mac_addresses = output[mac_start:mac_end].strip()
-        else:
-            mac_addresses = 'Unknown'
-
-        output_file.write("MAC addresses on {0}:\n{1}\n".format(device, mac_addresses))
+        mac_addresses = output[output.find('mac address-table'):output.find('Type:')].strip()
 
         # Retrieve the IP ARP table
-        arp_start = output.find('ip arp')
-        if arp_start != -1:
-            arp_table = output[arp_start + len('ip arp'):].strip()
-        else:
-            arp_table = 'Unknown'
+        arp_table = output[output.find('ip arp'):].strip()
 
+        # Write the output to the file
+        output_file.write("Hostname of {0}: {1}\n".format(device, hostname))
+        output_file.write("MAC addresses on {0}:\n{1}\n".format(device, mac_addresses))
         output_file.write("IP ARP table on {0}:\n{1}\n".format(device, arp_table))
 
         # Close the SSH connection
@@ -82,6 +57,7 @@ for device in devices:
         output_file.write("Failed to authenticate to {0}\n".format(device))
     except paramiko.SSHException as e:
         output_file.write("SSH error occurred while connecting to {0}: {1}\n".format(device, str(e)))
+
 
 # Close the output file
 output_file.close()
