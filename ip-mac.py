2f104baeaa170060a1ecc2fd7ec2888e10639c91
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-28 22:27:25 -0400

    Update ip-mac.py

--- ip-mac.py
+++ ip-mac.py
@@ -1,3 +1,4 @@
+import getpass
 import threading
 import paramiko
 
@@ -10,23 +11,27 @@ def save_device_info(device_info):
         f.write(device_info)
         f.write('\n\n')
 
-def get_cisco_ios_info(device):
-    """Retrieve hostname, MAC addresses, and ARP table from a Cisco IOS device."""
+def get_device_info(device):
+    """Retrieve hostname, MAC addresses, and ARP table from a Cisco device."""
     hostname = device['hostname']
     username = device['username']
     password = device['password']
-    enable_password = device['enable_password']
 
     try:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname, username=username, password=password)
 
-        # Enable privilege mode if an enable password is provided
-        if enable_password:
-            enable_cmd = f'enable\n{enable_password}\n'
-            client.send(enable_cmd)
-            client.recv(1000)  # Read the prompt
+        # Determine device type based on the prompt symbol
+        stdin, stdout, stderr = client.exec_command('')
+        prompt = stdout.read().decode('utf-8')
+
+        if ">" in prompt:
+            device_type = "cisco_ios"
+        elif "#" in prompt:
+            device_type = "cisco_nxos"
+        else:
+            device_type = "unknown"
 
         # Run show commands
         commands = [
@@ -48,45 +53,7 @@ def get_cisco_ios_info(device):
 
         client.close()
 
-        print(f'Retrieved information from {hostname}.')
-
-    except paramiko.AuthenticationException:
-        print(f'Authentication failed for {hostname}.')
-    except paramiko.SSHException as e:
-        print(f'Error occurred while connecting to {hostname}: {str(e)}')
-
-def get_cisco_nxos_info(device):
-    """Retrieve hostname, MAC addresses, and ARP table from a Cisco Nexus device."""
-    hostname = device['hostname']
-    username = device['username']
-    password = device['password']
-
-    try:
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname, username=username, password=password)
-
-        # Run show commands
-        commands = [
-            'show hostname',
-            'show mac address-table',
-            'show ip arp',
-        ]
-
-        device_info = f'Device: {hostname}\n\n'
-
-        for command in commands:
-            stdin, stdout, stderr = client.exec_command(command)
-            output = stdout.read().decode('utf-8')
-            device_info += f'Command: {command}\n'
-            device_info += output
-            device_info += '\n'
-
-        save_device_info(device_info)
-
-        client.close()
-
-        print(f'Retrieved information from {hostname}.')
+        print(f'Retrieved information from {hostname}. Device Type: {device_type}')
 
     except paramiko.AuthenticationException:
         print(f'Authentication failed for {hostname}.')
@@ -94,18 +61,11 @@ def get_cisco_nxos_info(device):
         print(f'Error occurred while connecting to {hostname}: {str(e)}')
 
 def process_device(device):
-    """Process a single device based on its device type."""
-    device_type = device.get('device_type')
-    if device_type == 'cisco_ios':
-        get_cisco_ios_info(device)
-    elif device_type == 'cisco_nxos':
-        get_cisco_nxos_info(device)
-    else:
-        print(f'Unknown device type for {device["hostname"]}')
+    """Process a single device."""
+    get_device_info(device)
 
 def main():
-    # Read device information
-        # Read device information from the file
+    # Read device information from the file
     with open('device_list.txt', 'r') as f:
         device_lines = f.read().splitlines()
 
@@ -115,13 +75,13 @@ def main():
     # Parse device information from each line
     for line in device_lines:
         if line.strip():  # Skip empty lines
-            hostname, device_type = line.split(',')
+            hostname, _ = line.split(',')
+            username = input(f"Enter username for {hostname}: ")
+            password = getpass.getpass(f"Enter password for {hostname}: ")
             devices.append({
                 'hostname': hostname.strip(),
-                'username': 'your_username',
-                'password': 'your_password',
-                'device_type': device_type.strip(),
-                'enable_password': 'your_enable_password'
+                'username': username.strip(),
+                'password': password.strip(),
             })
 
     # Create a list to store threads
