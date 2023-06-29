3cf4cd0ef837cde750be27502d53258f5b05968b
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-28 22:06:58 -0400

    Update ip-mac.py

--- ip-mac.py
+++ ip-mac.py
@@ -1,113 +1,142 @@
-import paramiko
 import threading
-import csv
-import re
+import paramiko
 
-class DeviceConnector(threading.Thread):
-    def __init__(self, ip, username, password, enable_password, output_lock):
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.username = username
-        self.password = password
-        self.enable_password = enable_password
-        self.output_lock = output_lock
-    
-    def run(self):
-        print "Connecting to", self.ip
-        try:
-            ssh_client = paramiko.SSHClient()
-            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh_client.connect(self.ip, username=self.username, password=self.password)
+# Define the filename to save the output
+output_filename = 'device_info.txt'
 
-            # Create an interactive shell session
-            remote_conn = ssh_client.invoke_shell()
-            
-            # Send the enable command
-            remote_conn.send("enable\n")
-            while not remote_conn.recv_ready():
-                pass
-            remote_conn.send("{}\n".format(self.enable_password))
+def save_device_info(device_info):
+    """Save device information to a file."""
+    with open(output_filename, 'a') as f:
+        f.write(device_info)
+        f.write('\n\n')
 
-            # Get hostname from command prompt
-            output = remote_conn.recv(1000)
-            hostname = output.split("#")[0].strip()
+def get_cisco_ios_info(device):
+    """Retrieve hostname, MAC addresses, and ARP table from a Cisco IOS device."""
+    hostname = device['hostname']
+    username = device['username']
+    password = device['password']
+    enable_password = device['enable_password']
 
-            print "Connected to", self.ip, "(hostname:", hostname, ")"
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, username=username, password=password)
 
-            # Get MAC addresses of physical interfaces
-            remote_conn.send('show mac address-table\n')
-            while not remote_conn.recv_ready():
-                pass
-            mac_address_output = remote_conn.recv(5000)
+        # Enable privilege mode if an enable password is provided
+        if enable_password:
+            enable_cmd = f'enable\n{enable_password}\n'
+            client.send(enable_cmd)
+            client.recv(1000)  # Read the prompt
 
-            # Get ARP table
-            remote_conn.send('show ip arp\n')
-            while not remote_conn.recv_ready():
-                pass
-            arp_table = remote_conn.recv(5000)
+        # Run show commands
+        commands = [
+            'show run | include hostname',
+            'show mac address-table',
+            'show ip arp',
+        ]
 
-            # Regex to match physical interface patterns
-            interface_pattern = re.compile(r'\D+\d+((/\d+)+(\.\d+)?)')
+        device_info = f'Device: {hostname}\n\n'
 
-            # Parse the information
-            for line in mac_address_output.split('\n'):
-                parts = line.split()
-                if len(parts) > 4:
-                    interface = parts[3]
-                    mac_address = parts[1]
-                    print "Debug: Processing line:", line  # Debug print statement
-                    
-                    if interface_pattern.match(interface):
-                        # Find IP address from ARP table
-                        ip_address_from_arp = "Not Found"
-                        for arp_line in arp_table.split('\n'):
-                            if mac_address in arp_line:
-                                ip_address_from_arp = arp_line.split()[1]
-                                break
-                        
-                        # Output the information
-                        with self.output_lock:
-                            with open('output.csv', 'a') as file:
-                                writer = csv.writer(file)
-                                writer.writerow([hostname, self.ip, interface, mac_address, ip_address_from_arp])
-                                print "Data written for", self.ip
-                    else:
-                        print "Interface pattern not matched:", interface
-                else:
-                    print "Line does not have enough parts:", line
-            
-            ssh_client.close()
+        for command in commands:
+            stdin, stdout, stderr = client.exec_command(command)
+            output = stdout.read().decode('utf-8')
+            device_info += f'Command: {command}\n'
+            device_info += output
+            device_info += '\n'
 
-        except Exception as e:
-            print "Error connecting to", self.ip, ":", str(e)
+        save_device_info(device_info)
 
-# Read device IPs from file
-with open("device_list.txt", "r") as file:
-    device_ips = file.readlines()
+        client.close()
 
-# Define credentials
-username = 'your_username'
-password = 'your_password'
-enable_password = 'your_enable_password'
+        print(f'Retrieved information from {hostname}.')
 
-# Lock to synchronize output to file
-output_lock = threading.Lock()
+    except paramiko.AuthenticationException:
+        print(f'Authentication failed for {hostname}.')
+    except paramiko.SSHException as e:
+        print(f'Error occurred while connecting to {hostname}: {str(e)}')
 
-# Create the output.csv file with headers
-with open('output.csv', 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow(['hostname', 'IP address of the switch', 'interface', 'mac address', 'ip address from arp'])
-    print "Created output.csv file with headers"
+def get_cisco_nxos_info(device):
+    """Retrieve hostname, MAC addresses, and ARP table from a Cisco Nexus device."""
+    hostname = device['hostname']
+    username = device['username']
+    password = device['password']
 
-# Create and start threads
-threads = []
-for ip in device_ips:
-    connector = DeviceConnector(ip.strip(), username, password, enable_password, output_lock)
-    threads.append(connector)
-    connector.start()
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, username=username, password=password)
 
-# Wait for all threads to complete
-for t in threads:
-    t.join()
+        # Run show commands
+        commands = [
+            'show hostname',
+            'show mac address-table',
+            'show ip arp',
+        ]
 
-print "Data collection complete"
+        device_info = f'Device: {hostname}\n\n'
+
+        for command in commands:
+            stdin, stdout, stderr = client.exec_command(command)
+            output = stdout.read().decode('utf-8')
+            device_info += f'Command: {command}\n'
+            device_info += output
+            device_info += '\n'
+
+        save_device_info(device_info)
+
+        client.close()
+
+        print(f'Retrieved information from {hostname}.')
+
+    except paramiko.AuthenticationException:
+        print(f'Authentication failed for {hostname}.')
+    except paramiko.SSHException as e:
+        print(f'Error occurred while connecting to {hostname}: {str(e)}')
+
+def process_device(device):
+    """Process a single device based on its device type."""
+    device_type = device.get('device_type')
+    if device_type == 'cisco_ios':
+        get_cisco_ios_info(device)
+    elif device_type == 'cisco_nxos':
+        get_cisco_nxos_info(device)
+    else:
+        print(f'Unknown device type for {device["hostname"]}')
+
+def main():
+        # Read device information from the file
+    with open('device_list.txt', 'r') as f:
+        device_lines = f.read().splitlines()
+
+    # Create a list to store device dictionaries
+    devices = []
+
+    # Parse device information from each line
+    for line in device_lines:
+        if line.strip():  # Skip empty lines
+            ip_address, username, password, device_type, enable_password = line.split(',')
+            devices.append({
+                'hostname': ip_address.strip(),
+                'username': username.strip(),
+                'password': password.strip(),
+                'device_type': device_type.strip(),
+                'enable_password': enable_password.strip(),
+            })
+
+    # Create a list to store threads
+    threads = []
+
+    # Process each device using multithreading
+    for device in devices:
+        t = threading.Thread(target=process_device, args=(device,))
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+    print('Script execution completed.')
+
+if __name__ == '__main__':
+    main()
