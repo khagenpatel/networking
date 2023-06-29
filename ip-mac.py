f990d3164dafc9ebd9bd56d640971097f72fdbe9
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-29 07:17:53 -0400

    Update ip-mac.py

--- ip-mac.py
+++ ip-mac.py
@@ -1,4 +1,5 @@
 import paramiko
+import time
 
 # Define the SSH credentials
 username = 'abcd'
@@ -25,29 +26,31 @@ for device in devices:
         stdin, stdout, stderr = ssh.exec_command('show run | include hostname')
         hostname = stdout.read().strip().split(' ')[1]
 
-        output_file.write("Hostname of {}: {}\n".format(device, hostname))
+        output_file.write("Hostname of {0}: {1}\n".format(device, hostname))
 
         # Show MAC addresses
         stdin, stdout, stderr = ssh.exec_command('show mac address-table')
+        time.sleep(1)  # Wait for the command to complete
         mac_addresses = stdout.read()
 
-        output_file.write("MAC addresses on {}:\n{}\n".format(device, mac_addresses))
+        output_file.write("MAC addresses on {0}:\n{1}\n".format(device, mac_addresses))
 
         # Show IP ARP table
         stdin, stdout, stderr = ssh.exec_command('show ip arp')
+        time.sleep(1)  # Wait for the command to complete
         arp_table = stdout.read()
 
-        output_file.write("IP ARP table on {}:\n{}\n".format(device, arp_table))
+        output_file.write("IP ARP table on {0}:\n{1}\n".format(device, arp_table))
 
         # Close the SSH connection
         ssh.close()
 
     except paramiko.AuthenticationException:
-        output_file.write("Failed to authenticate to {}\n".format(device))
+        output_file.write("Failed to authenticate to {0}\n".format(device))
     except paramiko.SSHException as e:
-        output_file.write("SSH error occurred while connecting to {}: {}\n".format(device, str(e)))
+        output_file.write("SSH error occurred while connecting to {0}: {1}\n".format(device, str(e)))
     except paramiko.ssh_exception.NoValidConnectionsError:
-        output_file.write("Unable to connect to {}\n".format(device))
+        output_file.write("Unable to connect to {0}\n".format(device))
 
 # Close the output file
 output_file.close()
