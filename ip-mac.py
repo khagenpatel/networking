eaec210a104c625824a8de9ddb6731199614072c
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-29 07:21:30 -0400

    Update ip-mac.py

--- ip-mac.py
+++ ip-mac.py
@@ -29,15 +29,19 @@ for device in devices:
         output_file.write("Hostname of {0}: {1}\n".format(device, hostname))
 
         # Show MAC addresses
-        stdin, stdout, stderr = ssh.exec_command('show mac address-table')
+        stdin, stdout, stderr = ssh.exec_command('show mac address-table', bufsize=-1)
         time.sleep(1)  # Wait for the command to complete
+        while not stdout.channel.exit_status_ready():
+            time.sleep(1)
         mac_addresses = stdout.read()
 
         output_file.write("MAC addresses on {0}:\n{1}\n".format(device, mac_addresses))
 
         # Show IP ARP table
-        stdin, stdout, stderr = ssh.exec_command('show ip arp')
+        stdin, stdout, stderr = ssh.exec_command('show ip arp', bufsize=-1)
         time.sleep(1)  # Wait for the command to complete
+        while not stdout.channel.exit_status_ready():
+            time.sleep(1)
         arp_table = stdout.read()
 
         output_file.write("IP ARP table on {0}:\n{1}\n".format(device, arp_table))
@@ -49,8 +53,6 @@ for device in devices:
         output_file.write("Failed to authenticate to {0}\n".format(device))
     except paramiko.SSHException as e:
         output_file.write("SSH error occurred while connecting to {0}: {1}\n".format(device, str(e)))
-    except paramiko.ssh_exception.NoValidConnectionsError:
-        output_file.write("Unable to connect to {0}\n".format(device))
 
 # Close the output file
 output_file.close()
