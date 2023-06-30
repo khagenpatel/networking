afecbd3c8f860b89acae4955d9ec74d001f642f4
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-29 21:15:48 -0400

    Update parse_mac.py

--- parse_mac.py
+++ parse_mac.py
@@ -1,6 +1,12 @@
 import re
 import csv
 
+def convert_mac_format(mac):
+    # Remove dots and convert to lower case
+    mac = mac.replace('.', '').lower()
+    # Insert colons
+    return ':'.join(mac[i:i + 2] for i in range(0, len(mac), 2))
+
 # Regular expressions for parsing the MAC address table entries
 hostname_regex = re.compile(r'==== (.+?) ====')
 mac_entry_regex = re.compile(r'\s*\S+\s+([0-9a-fA-F]{4}\.[0-9a-fA-F]{4}\.[0-9a-fA-F]{4})\s+\S+\s+(?:\S+\s+)*([\w/]+)')
@@ -14,7 +20,7 @@ with open('mac_addresses.csv', 'w', newline='') as csvfile:
     hostname = None
 
     # Read the notepad file line by line
-    with open('mac_table.txt', 'r') as file:
+    with open('switch_info.txt', 'r') as file:
         for line in file:
             # Check for hostname
             hostname_match = hostname_regex.match(line)
@@ -25,6 +31,8 @@ with open('mac_addresses.csv', 'w', newline='') as csvfile:
             mac_entry_match = mac_entry_regex.match(line)
             if mac_entry_match and hostname:
                 mac_address, interface = mac_entry_match.groups()
+                # Convert the MAC address format
+                mac_address = convert_mac_format(mac_address)
                 writer.writerow([hostname, mac_address, interface])
 
 print("MAC address information saved to mac_addresses.csv")
