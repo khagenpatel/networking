ebf7798b0bf13dea1a37946295c6eeefa81e789d
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-29 21:09:31 -0400

    Update parse_mac.py

--- parse_mac.py
+++ parse_mac.py
@@ -3,7 +3,7 @@ import csv
 
 # Regular expressions for parsing the MAC address table entries
 hostname_regex = re.compile(r'==== (.+?) ====')
-mac_entry_regex = re.compile(r'\s*\S+\s+([0-9a-fA-F]{4}\.[0-9a-fA-F]{4}\.[0-9a-fA-F]{4})\s+\S+(?:\s+\S+)*\s+([\w/]+)')
+mac_entry_regex = re.compile(r'\s*\S+\s+([0-9a-fA-F]{4}\.[0-9a-fA-F]{4}\.[0-9a-fA-F]{4})\s+\S+\s+(?:\S+\s+)*([\w/]+)')
 
 # Read the notepad file
 with open('switch_info.txt', 'r') as file:
@@ -17,10 +17,11 @@ with open('mac_addresses.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['hostname', 'mac_address', 'interface'])
 
+    # Iterate over the matched groups (hostname and corresponding MAC entries)
     for i in range(0, len(matches), 2):
         hostname = matches[i].strip()
         mac_section = matches[i + 1]
-        
+
         # Find MAC address entries
         for mac_entry in mac_entry_regex.findall(mac_section):
             mac_address, interface = mac_entry
