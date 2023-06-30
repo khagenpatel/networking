d139ca2e9d67102ba59b51e8b47ee372f9bc1e98
Khagen Patel <68674118+khagenpatel@users.noreply.github.com>
2023-06-29 21:12:47 -0400

    Update parse_mac.py

--- parse_mac.py
+++ parse_mac.py
@@ -5,26 +5,26 @@ import csv
 hostname_regex = re.compile(r'==== (.+?) ====')
 mac_entry_regex = re.compile(r'\s*\S+\s+([0-9a-fA-F]{4}\.[0-9a-fA-F]{4}\.[0-9a-fA-F]{4})\s+\S+\s+(?:\S+\s+)*([\w/]+)')
 
-# Read the notepad file
-with open('switch_info.txt', 'r') as file:
-    content = file.read()
-
-# Parse the content
-matches = hostname_regex.split(content)[1:]
-
 # Open CSV file to save the output
 with open('mac_addresses.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['hostname', 'mac_address', 'interface'])
 
-    # Iterate over the matched groups (hostname and corresponding MAC entries)
-    for i in range(0, len(matches), 2):
-        hostname = matches[i].strip()
-        mac_section = matches[i + 1]
+    # Initialize hostname
+    hostname = None
 
-        # Find MAC address entries
-        for mac_entry in mac_entry_regex.findall(mac_section):
-            mac_address, interface = mac_entry
-            writer.writerow([hostname, mac_address, interface])
+    # Read the notepad file line by line
+    with open('mac_table.txt', 'r') as file:
+        for line in file:
+            # Check for hostname
+            hostname_match = hostname_regex.match(line)
+            if hostname_match:
+                hostname = hostname_match.group(1).strip()
+
+            # Check for MAC address entry
+            mac_entry_match = mac_entry_regex.match(line)
+            if mac_entry_match and hostname:
+                mac_address, interface = mac_entry_match.groups()
+                writer.writerow([hostname, mac_address, interface])
 
 print("MAC address information saved to mac_addresses.csv")
